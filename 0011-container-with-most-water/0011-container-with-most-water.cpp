de9a4f03f0efdd9b1a655d5a7eceb61515c42755
Koushik Bhowmick <133016615+devv-koushik@users.noreply.github.com>
2026-09-26 10:41:56 +0530

    Time: 4 ms (20.5%), Space: 63 MB (10.87%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,26 +1,19 @@
-#include <vector>
-#include <algorithm>
-
 class Solution {
 public:
-    int maxArea(std::vector<int>& height) {
+    int maxArea (vector<int> & height){
         int left = 0;
         int right = height.size() - 1;
         int max_water = 0;
 
         while (left < right) {
-            int current_height = std::min(height[left], height[right]);
+            int current_height = min(height[left], height[right]);
             int current_width = right - left;
-            max_water = std::max(max_water, current_width * current_height);
+            max_water = max(max_water, current_width * current_height);
 
-            // Shift the pointer with the smaller height
-            if (height[left] < height[right]) {
-                left++;
-            } else {
-                right--;
-            }
+            height[left] < height[right] ? left++ : right --;
         }
 
         return max_water;
     }
+    
 };
